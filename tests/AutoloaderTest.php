549256d52499049055d6581d069f69125f76f03a
Natan Felles <natanfelles@gmail.com>
2021-06-27 11:14:18 -0300

    Coding Standard: Add header comments

--- tests/AutoloaderTest.php
+++ tests/AutoloaderTest.php
@@ -1,4 +1,13 @@
-<?php namespace Tests\Autoload;
+<?php
+/*
+ * This file is part of The Framework Autoload Library.
+ *
+ * (c) Natan Felles <natanfelles@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace Tests\Autoload;
 
 use Framework\Autoload\Autoloader;
 use PHPUnit\Framework\TestCase;
